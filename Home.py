baa7904d28d70a63adb48c62ece19be497914b11
universe95 <hejin0909@gmail.com>
2025-08-14 10:32:24 +0900

    Add files via upload

---
+++ Home.py
@@ -0,0 +1,16 @@
+import streamlit as st
+
+st.set_page_config(page_title="멀티페이지 웹", layout="wide")
+
+st.title("반도체 품질 관리 시스템")
+
+st.markdown(
+    "[📎 Tableau 대시보드 열기](https://prod-apnortheast-a.online.tableau.com/t/teamsparta/views/_17548742749860/1_1?:origin=card_share_link)",
+    unsafe_allow_html=True
+)
+
+image_path = "공정 대시보드.png"
+
+st.image(image_path, use_container_width=True)  
+
+
